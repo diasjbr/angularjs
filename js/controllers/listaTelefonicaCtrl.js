50e024ce9649e49f01a002e7190525c94f4278ed
Alexandre <dias.jbr@gmail.com>
2017-08-18 16:29:24 -0300

    novas pastas

---
+++ js/controllers/listaTelefonicaCtrl.js
@@ -0,0 +1,39 @@
+
+		angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function ($scope, uppercaseFilter) {
+			$scope.app = "Lista Telefonica";
+			$scope.contatos = [
+				{nome: uppercaseFilter("Joel"), telefone: "9999-8888", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
+				{nome: "Thalita", telefone: "9999-8877", data: new Date(), operadora: {nome: "Claro", codigo: 15, categoria: "Celular"}},
+				{nome: "Marianna", telefone: "9999-8866", data: new Date(), operadora: {nome: "GVT", codigo: 21, categoria: "Fixo"}},
+				{nome: "Alex", telefone: "9999-8855", data: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}}
+			];
+			$scope.operadoras = [
+				{nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
+				{nome: "Claro", codigo: 15, categoria: "Celular", preco: 1},
+				{nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
+				{nome: "Oi Fixo", codigo: 25, categoria: "Fixo", preco: 1},
+				{nome: "Vivo Fixo", codigo: 21, categoria: "Fixo", preco: 2},
+				{nome: "NET", codigo: 21, categoria: "Fixo", preco: 1},
+				{nome: "GVT", codigo: 21, categoria: "Fixo", preco: 4}
+			];
+			$scope.adicionarContato = function (contato) {
+				$scope.contatos.push(angular.copy(contato));
+				delete $scope.contato;
+				$scope.contatoForm.$setPristine();
+			};
+			$scope.apagarContatos = function (contatos) {
+				$scope.contatos = contatos.filter(function (contato) {
+					if (!contato.selecionado) return contato;
+				});
+			};
+			$scope.isContatoSelecionado = function (contatos) {
+				return contatos.some(function (contato) {
+					return contato.selecionado;
+				});
+			};
+			$scope.ordenarPor = function (campo) {
+				$scope.criterioDeOrdenacao = campo;
+				$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+			};
+
+		});
